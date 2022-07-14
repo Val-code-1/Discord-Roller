dd83fd51e9a06c43240d2fcc24c7c3cf5346ff15
Val-code-1 <reit.ben@gmail.com>
2022-07-14 01:41:40 -0500

    Working prototype ready

--- Commands/roll.js
+++ Commands/roll.js
@@ -12,13 +12,49 @@ module.exports = {
     if (args[0]) {
       let amount = parseInt(args[0]);
       let resultMSG = [];
+      let rerollDice = 0;
+      let successes = 0;
+      let rerollResultMSG = [];
+      let rerollSuccesses = 0;
       for (let i = 0; i < amount; i++) {
         let random = Math.random();
         let result = Math.floor(random * 10 + 1);
+        if (result >= args[1]) {
+          successes++;
+        } else if (result === 1) {
+          successes--;
+        }
+        if (result === 10) {
+          rerollDice++;
+          if (args[2]) {
+            successes++;
+          }
+        }
         resultMSG.push(result);
       }
+
+      if (rerollDice > 0) {
+        for (let i = 0; i < rerollDice; i++) {
+          let random = Math.random();
+          let result = Math.floor(random * 10 + 1);
+          if (result >= args[1]) {
+            rerollSuccesses++;
+          }
+          rerollResultMSG.push(result);
+        }
+      }
       let cleanMSG = resultMSG.join(", ");
-      return message.channel.send([`${cleanMSG} `, `${message.author}`]);
+      let cleanRerollResultMSG = rerollResultMSG.join(", ");
+
+      return message.channel.send([
+        `${message.author} rolls...`,
+        `${cleanMSG} `,
+        `Successes = ${successes}`,
+        `Reroll Dice = ${rerollDice}`,
+        `Reroll Result = ${cleanRerollResultMSG}`,
+        `Reroll Successes = ${rerollSuccesses}`,
+        `Total Successes = ${successes + rerollSuccesses}`,
+      ]);
     }
   },
 };
